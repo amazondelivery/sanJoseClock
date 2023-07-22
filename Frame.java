e860df3d2ccdfdd05c74334b9681423a8d0aed5f
Vikram Balaji <mr.vikram.balaji@gmail.com>
2023-07-22 14:53:08 -0700

    tried to fix the window

--- Frame.java
+++ Frame.java
@@ -1,12 +1,28 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
 
 public class Frame extends JFrame {
+
+    /**
+     * Thank you Coderguy194 from CodeProject.com for helping me make
+    undecorated windows moveable.
+     */
+
+    int posX = 0;
+    int posY = 0;
+
     public Frame() {
-       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       this.setSize(1250, 500);
-       this.setLocationRelativeTo(null);
-       this.setVisible(true);
+        this.setUndecorated(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1250, 500);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setVisible(true);
+        //this.addMouseListener(new MouseAdapter() {
+
+        //}
+        this.setTitle("San Jose's Clock");
     }
 
     public void paint(Graphics graph) {
